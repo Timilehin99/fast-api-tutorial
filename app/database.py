90104a0f02c3748100fb0c85fb5d19f89b3cfd94
WintersShadow12 <timilearnsprogramming@gmail.com>
2023-02-23 18:40:04 +0100

    Fixed small mistakes in the db.

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}'
 
 
 try:
